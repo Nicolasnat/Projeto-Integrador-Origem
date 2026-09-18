a9a7062a11cd65fcee9247aed4e52ec65a8ec6dc
Nicolasnat <nicolas.natario0@gmail.com>
2026-09-18 14:52:23 -0300

    criado o Header

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,26 +1,10 @@
+import Header from "@/app/components/ui/Header";
 import { Box, Button, Heading, Text, VStack } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Box minH="100vh" bg="gray.50">
-      <VStack
-        minH="100vh"
-        justify="center"
-        gap={6}
-        px={6}
-      >
-        <Heading size="2xl">
-          Projeto Origem
-        </Heading>
-
-        <Text fontSize="lg" color="gray.600">
-          Nossa plataforma está começando.
-        </Text>
-
-        <Button colorPalette="blue" size="lg">
-          Começar
-        </Button>
-      </VStack>
-    </Box>
+    <div className="w-screen h-screen bg-orange-50">
+        <Header/>
+    </div>
   );
 }
